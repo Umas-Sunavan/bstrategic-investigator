78b20889699f8086ce5101b479a3af03a558916f
Umas-Sunavan <30775199+Umas-Sunavan@users.noreply.github.com>
2021-07-29 21:12:34 +0800

    init pages and apply Xian's code

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './category/category.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: IntroductionComponent
+  },
+  {
+    path: 'category',
+    component: CategoryComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { CategoryComponent } from './category/category.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IntroductionComponent,
+    CategoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
